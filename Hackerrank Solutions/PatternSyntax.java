afa450a1283641a6ee00c799f7c4982a32c11a0d
LAKSH DOSHI <102138784+LAKSH3002@users.noreply.github.com>
2023-06-15 23:30:47 +0530

    Create PatternSyntax.java

---
+++ Hackerrank Solutions/PatternSyntax.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0)
+        {
+        String pattern = in.nextLine();
+        try{
+            Pattern p = Pattern.compile(pattern);
+            // if(Pattern.matches(p, pattern))
+            System.out.println("valid");
+        }
+        catch(PatternSyntaxException e)
+        {
+            System.out.println("Invalid");
+        }
+        testCases--;               
+        }
+        in.close();
+    }
+}
+
+
+
